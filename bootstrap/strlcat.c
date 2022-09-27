72ab8318f329ba1ec473b9098c76da050e59b0b6
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2022-09-27 16:55:02 -0500

    fix linux build: strlcat, strlcpy

--- bootstrap/strlcat.c
+++ bootstrap/strlcat.c
@@ -33,10 +33,10 @@
 static char *rcsid = "$OpenBSD: strlcat.c,v 1.2 1999/06/17 16:28:58 millert Exp $";
 #endif /* LIBC_SCCS and not lint */
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
+/* obazl __FBSDID("$FreeBSD$"); */
 
 #include <sys/types.h>
-/* #include <sys/libkern.h> */
+/* obazl #include <sys/libkern.h> */
 
 /*
  * Appends src to string dst of size siz (unlike strncat, siz is the
--- bootstrap/strlcpy.c
+++ bootstrap/strlcpy.c
@@ -17,10 +17,10 @@
  */
 
 #include <sys/cdefs.h>
-/* __FBSDID("$FreeBSD$"); */
+/* obazl __FBSDID("$FreeBSD$"); */
 
 #include <sys/types.h>
-/* #include <sys/libkern.h> */
+/* obazl #include <sys/libkern.h> */
 
 /*
  * Copy string src to buffer dst of size dsize.  At most dsize-1
